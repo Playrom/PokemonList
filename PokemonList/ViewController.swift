70173f28d258bcd19395dea159ee548cdf076786
Giorgio Romano <lavoro@giorgioromano.it>
2020-05-11 10:34:41 +0200

    Initial Commit

---
+++ PokemonList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokemonList
+//
+//  Created by Giorgio Romano on 11/05/2020.
+//  Copyright © 2020 Giorgio Romano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
